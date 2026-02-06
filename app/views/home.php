59f140b444ef0d82e37d1e53ea58995add9db0b8
= <=>
2026-02-06 16:01:42 +0100

    Add n8n to the libraries in footer

--- app/views/home.php
+++ app/views/home.php
@@ -1,20 +1,20 @@
 <div class="flex flex-col flex-1 items-center text-center min-h-max max-w-screen justify-around">
-		<h1 class="text-5xl font-bold">Welcome to Migros Helper!</h1>
-		<p class="text-2xl w-2xl max-w-screen  min-h-max">
-			This project is an AI chat app made to create custome grocery lists
-			from the user's needs using Migros' data
-		</p>
-		<div class="flex h-94 w-full justify-between flex-col bg-base-200 p-3 rounded">
-			<div class="mb-3 overflow-y-scroll">
-				<div class="bg-primary w-fit p-2 rounded md-3">sss</div>
-				<div class="bg-secondary w-fit p-2 rounded mb-3 ml-auto">
-					Hello
-				</div>
-				<div class="bg-secondary w-fit p-2 rounded mb-3 ml-auto">SSS</div>
+	<h1 class="text-5xl font-bold">Welcome to Migros Helper!</h1>
+	<p class="text-2xl w-2xl max-w-screen  min-h-max">
+		This project is an AI chat app made to create custome grocery lists
+		from the user's needs using Migros' data
+	</p>
+	<div class="flex h-94 w-full justify-between flex-col bg-base-200 p-3 rounded">
+		<div class="mb-3 overflow-y-scroll">
+			<div class="bg-primary w-fit p-2 rounded md-3">sss</div>
+			<div class="bg-secondary w-fit p-2 rounded mb-3 ml-auto">
+				Hello
 			</div>
-			<div class="flex">
-				<input type="text" placeholder="Type here" class="input w-full mr-3" />
-				<button class="btn btn-primary">Send <i class="bi bi-send-fill"></i></button>
-			</div>
+			<div class="bg-secondary w-fit p-2 rounded mb-3 ml-auto">SSS</div>
 		</div>
+		<div class="flex">
+			<input type="text" placeholder="Type here" class="input w-full mr-3" />
+			<button class="btn btn-primary">Send <i class="bi bi-send-fill"></i></button>
+		</div>
+	</div>
 </div>
--- app/views/layout.php
+++ app/views/layout.php
@@ -10,35 +10,36 @@
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
 <body class="min-h-screen flex flex-col">
-	<header class="sticky top-0 z-1">
+    <header class="sticky top-0 z-1">
         <nav class="navbar bg-base-100 shadow-sm">
             <a class="btn btn-ghost text-xl" href="/">Mirgos Helper</a>
         </nav>
     </header>
-	<main class="m-auto relative flex flex-1">
+    <main class="m-auto relative flex flex-1">
         <?= $content ?>
     </main>
-	<footer class="footer sm:footer-horizontal bg-neutral text-neutral-content p-10">
-		<nav>
-			<h6 class="footer-title">Authors</h6>
-			<a class="link link-hover" href="https://github.com/NoodraTV">Nicolas Bieri</a>
-			<a class="link link-hover" href="https://github.com/Ethjuro">Ethan Raphael</a>
-			<a class="link link-hover" href="https://github.com/layode-ch">Joao Victor Pereira Vaz</a>
-		</nav>
-		<nav>
-			<h6 class="footer-title">Frontend</h6>
-			<a class="link link-hover" href="https://daisyui.com/">DaisyUI</a>
-			<a class="link link-hover" href="https://tailwindcss.com/">Tailwind</a>
-		</nav>
-		<nav>
-			<h6 class="footer-title">Backend</h6>
-			<a class="link link-hover" href="https://www.slimframework.com/">Slim Framework V4</a>
-			<a class="link link-hover" href="https://github.com/Seldaek/monolog">Monolog</a>
-		</nav>
-		<nav>
-			<h6 class="footer-title">Legal</h6>
-			<a class="link link-hover" href="https://creativecommons.org/licenses/by-nc/4.0/">Licence CC BY-NC V4.0</a>
-		</nav>
-	</footer>
+    <footer class="footer sm:footer-horizontal bg-neutral text-neutral-content p-10">
+        <nav>
+            <h6 class="footer-title">Authors</h6>
+            <a class="link link-hover" href="https://github.com/NoodraTV">Nicolas Bieri</a>
+            <a class="link link-hover" href="https://github.com/Ethjuro">Ethan Raphael</a>
+            <a class="link link-hover" href="https://github.com/layode-ch">Joao Victor Pereira Vaz</a>
+        </nav>
+        <nav>
+            <h6 class="footer-title">Frontend</h6>
+            <a class="link link-hover" href="https://daisyui.com/">DaisyUI</a>
+            <a class="link link-hover" href="https://tailwindcss.com/">Tailwind</a>
+        </nav>
+        <nav>
+            <h6 class="footer-title">Backend</h6>
+            <a class="link link-hover" href="https://www.slimframework.com/">Slim Framework V4</a>
+            <a class="link link-hover" href="https://github.com/Seldaek/monolog">Monolog</a>
+            <a class="link link-hover" href="https://n8n.io/">n8n</a>
+        </nav>
+        <nav>
+            <h6 class="footer-title">Legal</h6>
+            <a class="link link-hover" href="https://creativecommons.org/licenses/by-nc/4.0/">Licence CC BY-NC V4.0</a>
+        </nav>
+    </footer>
 </body>
 </html>
